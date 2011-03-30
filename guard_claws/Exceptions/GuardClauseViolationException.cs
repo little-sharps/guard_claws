3a391b881b35a62b847ff8b2c368200f42c17b83
Brendan Erwin <brendanjerwin@gmail.com>
2011-03-30 16:03:08 -0400

    Fixed a stupid typo.

--- guard_claws/Exceptions/GuardClauseViolationException.cs
+++ guard_claws/Exceptions/GuardClauseViolationException.cs
@@ -4,14 +4,15 @@ namespace GuardClaws.Exceptions
 {
     public class GuardClauseViolationException<T> : ApplicationException
     {
-        protected GuardClauseViolationException(Func<T> delinquent , string message) : base(BuildMessage(delinquent, message))
+        protected GuardClauseViolationException(Func<T> delinquent, string message)
+            : base(BuildMessage(delinquent, message))
         {
             NameOfDelinquent = Reflect.VariableName(delinquent);
         }
 
         static string BuildMessage(Func<T> delinquent, string message)
         {
-            return string.Format("{0}/n/nDelinquent: {1}", message, Reflect.VariableName(delinquent)); 
+            return string.Format("{0}\n\nDelinquent: {1}", message, Reflect.VariableName(delinquent));
         }
 
         public string NameOfDelinquent { get; private set; }
