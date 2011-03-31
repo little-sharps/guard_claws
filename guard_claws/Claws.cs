3fa8fc4b5a475be61dd12b74066ad528c21d6a3c
Brendan Erwin <brendanjerwin@gmail.com>
2011-03-31 12:59:19 -0400

    Fixes #2 - Assertion that takes a predicate lambda

--- guard_claws/Claws.cs
+++ guard_claws/Claws.cs
@@ -6,15 +6,22 @@ namespace GuardClaws
 {
     public static class Claws
     {
+        public static CallThrough<T> Ensure<T>(Func<T> variable)
+        {
+            return new CallThrough<T>(variable);
+        }
+
         public static void NotNull<T>(Func<T> variable) where T : class
         {
             if (variable.Invoke() != null) return;
             throw new VariableMustNotBeNullException<T>(variable);
         }
 
-        public static void NotNull<T>(Func<T?> variable) where T : struct{
+        public static void NotNull<T>(Func<T?> variable) where T : struct
+        {
             var instance = variable.Invoke();
-            if (!instance.HasValue){
+            if (!instance.HasValue)
+            {
                 throw new VariableMustNotBeNullException<T?>(variable);
             }
         }
@@ -23,15 +30,17 @@ namespace GuardClaws
         {
             NotNull(variable);
 
-            if(variable.Invoke() != string.Empty) return;
+            if (variable.Invoke() != string.Empty) return;
             throw new VariableMustNotBeBlankException(variable);
         }
 
-        public static void NotNullNotEmpty<T>(Func<T> variable) where T : class, IEnumerable {
+        public static void NotNullNotEmpty<T>(Func<T> variable) where T : class, IEnumerable
+        {
             NotNull(variable);
 
             var enumerator = variable.Invoke().GetEnumerator();
-            if (!enumerator.MoveNext()){
+            if (!enumerator.MoveNext())
+            {
                 throw new VariableMustNotBeEmptyException<T>(variable);
             }
         }
@@ -39,34 +48,55 @@ namespace GuardClaws
         public static void Numeric(Func<string> variable)
         {
             double junk;
-            if (double.TryParse(variable.Invoke(),out junk)) return;
+            if (double.TryParse(variable.Invoke(), out junk)) return;
             throw new VariableMustBeNumericException(variable);
         }
 
-        public static void NotDefault<T>(Func<T> variable) {
+        public static void NotDefault<T>(Func<T> variable)
+        {
             if (default(T) == null)
             {
-                if (!(variable.Invoke() == null)) return;    
+                if (!(variable.Invoke() == null)) return;
             }
-            else {
-                if (!variable.Invoke().Equals(default(T))) return;    
+            else
+            {
+                if (!variable.Invoke().Equals(default(T))) return;
             }
             throw new VariableMustNotBeDefaultValueException<T>(variable);
         }
 
-        public static void NotEqual<T>(Func<T> variable, T comparedTo) 
+        public static void NotEqual<T>(Func<T> variable, T comparedTo)
         {
-            if(!variable.Invoke().Equals(comparedTo)) return;
-            throw new VariableMustNotBeEqualException<T>(variable,comparedTo);
+            if (!variable.Invoke().Equals(comparedTo)) return;
+            throw new VariableMustNotBeEqualException<T>(variable, comparedTo);
         }
 
-        public static void AtLeast<T>(Func<T> variable, T comparedTo) where T:IComparable
+        public static void AtLeast<T>(Func<T> variable, T comparedTo) where T : IComparable
         {
             var val = variable.Invoke();
             if (val.Equals(comparedTo)) return;
-            if (((IComparable)val).CompareTo(comparedTo)>0) return;
+            if (((IComparable)val).CompareTo(comparedTo) > 0) return;
 
             throw new VariableMustBeAtLeastException<T>(variable, comparedTo);
+        }
+    }
+
+
+    public class CallThrough<T>
+    {
+        private readonly Func<T> variable;
+
+        public CallThrough(Func<T> variable)
+        {
+            this.variable = variable;
+        }
+
+        public void Passes(Predicate<T> predicate)
+        {
+            if (!predicate(variable.Invoke()))
+            {
+                throw new VariableMustPassProvidedPredicateException<T>(variable);
+            }
         }
     }
 }
