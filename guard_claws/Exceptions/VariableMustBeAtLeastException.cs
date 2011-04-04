6ea7fd580822dfb37ab3767a32ef68e45cc50a4b
Brendan Erwin <brendanjerwin@gmail.com>
2011-04-04 15:37:00 -0400

    These had messages that were exactly wrong...

--- guard_claws/Exceptions/VariableMustBeAtLeastException.cs
+++ guard_claws/Exceptions/VariableMustBeAtLeastException.cs
@@ -5,7 +5,7 @@ namespace GuardClaws.Exceptions
     public class VariableMustBeAtLeastException<T> : GuardClauseComparisonViolationException<T>
     {
         public VariableMustBeAtLeastException(Func<T> delinquent, T comparedTo)
-            : base(delinquent, comparedTo, "Variable must not be at least the provided comparison value.")
+            : base(delinquent, comparedTo, "Variable must be at least the provided comparison value.")
         {
         }
     }
--- guard_claws/Exceptions/VariableMustBeGreaterThanException.cs
+++ guard_claws/Exceptions/VariableMustBeGreaterThanException.cs
@@ -5,7 +5,7 @@ namespace GuardClaws.Exceptions
     public class VariableMustBeGreaterThanException<T> : GuardClauseComparisonViolationException<T>
     {
         public VariableMustBeGreaterThanException(Func<T> delinquent, T comparedTo)
-            : base(delinquent, comparedTo, "Variable must not be greater than the provided comparison value.")
+            : base(delinquent, comparedTo, "Variable must be greater than the provided comparison value.")
         {
         }
     }
