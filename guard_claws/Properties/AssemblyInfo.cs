58182b52fd13d95ca56a4dec5dd042cc74392a2f
Brendan Erwin <brendanjerwin@gmail.com>
2011-04-04 14:50:19 -0400

    Update version to 0.0.6.

--- guard_claws/Properties/AssemblyInfo.cs
+++ guard_claws/Properties/AssemblyInfo.cs
@@ -3,6 +3,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("guard_claws")]
 [assembly: AssemblyCopyright("Copyright (c) Brendan Erwin (and contributors) 2011")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5")]
+[assembly: AssemblyVersion("0.0.6")]
+[assembly: AssemblyFileVersion("0.0.6")]
 
